48b38e0b5586a4bf18e09d469f42111ddc54b674
Emil Hørlyck <eh@signifly.com>
2024-10-16 12:59:50 +0200

    feat: add increment and decrement

--- src/polyMetric.php
+++ src/polyMetric.php
@@ -39,4 +39,82 @@ class PolyMetric
 
         return $metric;
     }
+
+    public function increment($name, $value = 1, $day = null, $month = null, $year = null): array
+    {
+        $dailyMetric = $this->incrementDaily($name, $value, $day, $month, $year);
+        $monthlyMetric = $this->incrementMonthly($name, $value, $month, $year);
+
+        return [$dailyMetric, $monthlyMetric];
+    }
+
+    public function incrementDaily($name, $value = 1, $day = null, $month = null, $year = null): DailyMetric
+    {
+        $metric = DailyMetric::firstOrCreate(
+            [
+                'name' => $name,
+                'day' => $day ?? now()->day,
+                'month' => $month ?? now()->month,
+                'year' => $year ?? now()->year,
+            ]
+        );
+
+        $metric->increment('value', $value);
+
+        return $metric;
+    }
+
+    public function incrementMonthly($name, $value = 1, $month = null, $year = null): Metric
+    {
+        $metric = Metric::firstOrCreate(
+            [
+                'name' => $name,
+                'month' => $month ?? now()->month,
+                'year' => $year ?? now()->year,
+            ]
+        );
+
+        $metric->increment('value', $value);
+
+        return $metric;
+    }
+
+    public function decrement($name, $value = 1, $day = null, $month = null, $year = null): array
+    {
+        $dailyMetric = $this->decrementDaily($name, $value, $day, $month, $year);
+        $monthlyMetric = $this->decrementMonthly($name, $value, $month, $year);
+
+        return [$dailyMetric, $monthlyMetric];
+    }
+
+    public function decrementDaily($name, $value = 1, $day = null, $month = null, $year = null): DailyMetric
+    {
+        $metric = DailyMetric::firstOrCreate(
+            [
+                'name' => $name,
+                'day' => $day ?? now()->day,
+                'month' => $month ?? now()->month,
+                'year' => $year ?? now()->year,
+            ]
+        );
+
+        $metric->decrement('value', $value);
+
+        return $metric;
+    }
+
+    public function decrementMonthly($name, $value = 1, $month = null, $year = null): Metric
+    {
+        $metric = Metric::firstOrCreate(
+            [
+                'name' => $name,
+                'month' => $month ?? now()->month,
+                'year' => $year ?? now()->year,
+            ]
+        );
+
+        $metric->decrement('value', $value);
+
+        return $metric;
+    }
 }
