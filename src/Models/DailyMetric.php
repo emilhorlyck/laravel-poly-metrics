6b94c9de361d28f639653f352e8f96b09501422b
Emil Hørlyck <eh@signifly.com>
2024-10-16 12:59:34 +0200

    fix: remove functionc from model

--- src/Models/DailyMetric.php
+++ src/Models/DailyMetric.php
@@ -20,24 +20,4 @@ class DailyMetric extends Model
         'month',
         'year',
     ];
-
-    public static function incrementGeneralMetric(string $name, int $value = 1, $day = null, $month = null, $year = null, $excludeMonthlyMetric = false): void
-    {
-        DailyMetric::firstOrCreate(
-            [
-                'model_type' => null,
-                'name' => $name,
-                'day' => $day ?? now()->day,
-                'month' => $month ?? now()->month,
-                'year' => $year ?? now()->year,
-            ],
-            [
-                'value' => 0,
-            ]
-        )->increment('value', $value);
-
-        if (! $excludeMonthlyMetric) {
-            Metric::incrementGeneralMetric($name, $value, $day, $month, $year, true);
-        }
-    }
 }
--- src/Models/Metric.php
+++ src/Models/Metric.php
@@ -19,19 +19,4 @@ class Metric extends Model
         'month',
         'year',
     ];
-
-    public static function incrementGeneralMetric(string $name, int $value = 1, $day = null, $month = null, $year = null, $excludeMonthlyMetric = false): void
-    {
-        Metric::firstOrCreate(
-            [
-                'model_type' => null,
-                'name' => $name,
-                'month' => $month ?? now()->month,
-                'year' => $year ?? now()->year,
-            ],
-            [
-                'value' => 0,
-            ]
-        )->increment('value', $value);
-    }
 }
