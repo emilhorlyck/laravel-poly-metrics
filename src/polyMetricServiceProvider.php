f09a55c1e1c9a97ccb5bd78285f0cc0ea037c9c2
Emil Hørlyck <eh@signifly.com>
2024-10-16 10:35:51 +0200

    fix: naming of migration

--- src/polyMetricServiceProvider.php
+++ src/polyMetricServiceProvider.php
@@ -19,8 +19,8 @@ class PolyMetricServiceProvider extends PackageServiceProvider
             ->name('laravel-poly-metrics')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_daily_laravel_poly_metrics_table')
-            ->hasMigration('create_monthly_laravel_poly_metrics_table')
+            ->hasMigration('create_daily_poly_metrics_table')
+            ->hasMigration('create_monthly_poly_metrics_table')
             ->hasCommand(PolyMetricCommand::class);
     }
 }
