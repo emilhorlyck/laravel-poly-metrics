829f139e7b5acd14af9f175e1d997fdf98a2244a
Emil Hørlyck <eh@signifly.com>
2024-10-16 11:23:00 +0200

    feat(wip): set metric through facade

--- src/polyMetric.php
+++ src/polyMetric.php
@@ -4,8 +4,28 @@ namespace emilhorlyck\PolyMetric;
 
 class PolyMetric
 {
-    public function getDemoMetric()
+    public function set($name, $value, $month = null, $year = null): array
     {
-        return 'Demo Metric';
+        $metric = Metrics::Create([
+            'name' => $name,
+            'value' => $value,
+            'month' => $month ?? now()->month,
+            'year' => $year ?? now()->year,
+        ]);
+
+        return $metric->toArray();
+    }
+
+    public function setDaily($name, $value, $day = null, $month = null, $year = null)
+    {
+        $metric = DailyMetrics::Create([
+            'name' => $name,
+            'value' => $value,
+            'day' => $day ?? now()->day,
+            'month' => $month ?? now()->month,
+            'year' => $year ?? now()->year,
+        ]);
+
+        return $metric->toArray();
     }
 }
